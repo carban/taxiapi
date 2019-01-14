cde85ae6fee605a145c51cb0666018d03dc607af
carban <murillo.carlos@correounivalle.edu.co>
2019-01-13 20:33:25 -0500

    new commit

--- src/routes/map.js
+++ src/routes/map.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const Router = express.Router();
-const db = require('../config/db2.js');
+const db = require('../config/db.js');
 
 
 Router.get('/api/map/info', async (req, res) => {
   const myquery = {
-    text: "select id, (select ST_AsGeoJSON(geom)::json) as geom, name, tel from stores"
+    text: "select driver_serial, first_name, last_name, phone, (select ST_AsGeoJSON(coor)::json) as geom from drivers"
   }
   await db.query(myquery)
   .then(dbres => {
-    console.log('mapdata');
+    console.log('Drivers consulted');
     res.json({mapdata: dbres.rows});
   })
   .catch(err => {
