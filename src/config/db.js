75ffd24c6db547c7831efbbe3c0cc196bfa01f9d
carban <murillo.carlos@correounivalle.edu.co>
2019-04-13 23:34:40 -0500

    cambio de puerto

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const { Pool, Client } = require('pg');
-const connectionString = 'postgresql://postgres:lordpostgres123@localhost:5432/andresmon';
+const connectionString = 'postgresql://postgres:lordpostgres123@localhost:5430/andresmon';
 // const connectionString = 'postgresql://postgres:1234@localhost:5433/andresmon';
 const pool = new Pool({
   connectionString: connectionString,
